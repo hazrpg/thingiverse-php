b6e2ab244be3b5ca2e5076612a5266afc24564bc
Hassan Williamson <haz@hazrpg.co.uk>
2018-01-22 17:53:25 +0000

    Added inital work for thingisync tool which syncronises the collections of a user(s) to your hard drive.

---
+++ config.template.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * User: hazrpg
+ * Date: 19/01/18
+ * Time: 16:26
+ */
+
+define('FORCE_DOWNLOAD', false);
+define('DL_DIR', '/home/user/change/me/');
+define('DEBUG', false);
+define('DIR_PERMISSIONS', 0774);
